775939df7026e5de482cf8621e2da5a3925213fd
yang <yanggu123138@gmail.com>
2022-10-13 23:33:26 +0100

    DQN update

--- codes/DQN/DQN.py
+++ codes/DQN/DQN.py
@@ -81,6 +81,7 @@ class DQN(object):
         )
         self.target_q_net = deepcopy(self.q_net)
 
+    @torch.no_grad()
     def select_action(self, obs):
         q_values = self.q_net(torch.as_tensor(obs))
         action = q_values.max(dim=0)[1].item()
--- codes/DQN/task.py
+++ codes/DQN/task.py
@@ -1,82 +1,109 @@
 import gym
 import random
 import numpy as np
+from torch import save,load
 
 from DQN import DQN
 
 cfg = {
-    "episodes": 6000,
+    "episodes": 500,
     "n_time_step": 1000,
-    "epsilon_start": 1.0,
+    "epsilon_start": 0.5,
     "epsilon_end": 0.02,
-    "epsilon_decay": 100000
+    "epsilon_decay": 10000
 }
 agent_dict= {
-        "batch_size": 128,
+        "batch_size": 64,
         "gamma": 0.99,
         "learning_rate": 0.002,
         "buffer_size": 10000,
-        "update_frequency":20,
+        "update_frequency":30,
         "hidden_dim": 20,
         "layer_num": 5
     }
 
 
-env = gym.make('CartPole-v1', new_step_api=True)
-s = env.reset(seed=1)   
 
-agent = DQN(
-    env=env,
-    batch_size=agent_dict["batch_size"],
-    buffer_sizes=agent_dict["buffer_size"],
-    gamma=agent_dict["gamma"],
-    learning_rate=agent_dict["learning_rate"],
-    update_frequency=agent_dict["update_frequency"],
-    q_net_h_dim=agent_dict["hidden_dim"],
-    q_net_layer_num=agent_dict["layer_num"]
-)
 
-reward_buffer = np.empty(shape=cfg["episodes"])
+def train(env:gym.Env, agent:DQN, cfg):
+    s = env.reset(seed=1)  
+    reward_buffer = np.empty(shape=cfg["episodes"])
 
-STPES_BEFORE = 5
-for episode_i in range(cfg["episodes"]):
-    episode_reward = 0
-    for step_i in range(cfg["n_time_step"]):
+    for episode_i in range(cfg["episodes"]):
+        episode_reward = 0
+        for step_i in range(cfg["n_time_step"]):
 
-        epsilon = np.interp(
-            episode_i*cfg["episodes"] + step_i, 
-            [0, cfg["epsilon_decay"]], 
-            [cfg["epsilon_start"], cfg["epsilon_end"]]
-            )
-        
-        random_sample = random.random()
+            epsilon = np.interp(
+                episode_i*cfg["episodes"] + step_i, 
+                [0, cfg["epsilon_decay"]], 
+                [cfg["epsilon_start"], cfg["epsilon_end"]]
+                )
+            
+            random_sample = random.random()
 
-        if random_sample <= epsilon:
-            a = env.action_space.sample()
-        else:
-            a = agent.select_action(s) 
-        
-        s_, r, done, info,_ = env.step(a)
-        #print(a)
-        a = np.array(a).copy()
-        agent.memory.add(obs=s, action=a, done=done, reward=r, obs_=s_,infos=info) 
-        s = s_
-        episode_reward += r
-        
-        agent.update()
+            if random_sample <= epsilon:
+                a = env.action_space.sample()
+            else:
+                a = agent.select_action(s) 
+            
+            s_, r, done, info,_ = env.step(a)
+            #print(a)
+            a = np.array(a).copy()
+            agent.memory.add(obs=s, action=a, done=done, reward=r, obs_=s_,infos=info) 
+            s = s_
+            episode_reward += r
+            
+            agent.update()
+
+            if done:
+                s = env.reset()
+                reward_buffer[episode_i] = episode_reward
+                break
+            
+            if episode_reward>reward_buffer.max():
+                save(agent.q_net,'best_pt')
+
+        print(f"Episode: {episode_i}, reward: {episode_reward}")
+
+    env.close()
+    print("Finished!")
+
+
+def eval(env:gym.Env, agent:DQN):
+    agent.q_net = load('best_pt')
+    s = env.reset()
+    while True:
+        a = agent.select_action(s)
+        s_, r, done, info, _ = env.step(a) 
+        env.render()
 
         if done:
-            s = env.reset()
-            reward_buffer[episode_i] = episode_reward
+            env.reset()
             break
-        
-    print(f"Episode: {episode_i}, reward: {episode_reward}")
 
 
+def main():
+    env = gym.make('CartPole-v1', new_step_api=True) 
+
+    agent = DQN(
+        env=env,
+        batch_size=agent_dict["batch_size"],
+        buffer_sizes=agent_dict["buffer_size"],
+        gamma=agent_dict["gamma"],
+        learning_rate=agent_dict["learning_rate"],
+        update_frequency=agent_dict["update_frequency"],
+        q_net_h_dim=agent_dict["hidden_dim"],
+        q_net_layer_num=agent_dict["layer_num"]
+    )
+
+    print(agent.q_net)
+    #train(env, agent, cfg)
+    eval(env, agent)
+
+if __name__=="__main__":
+    main()
+    
 
 
-
-
-        
 
 
